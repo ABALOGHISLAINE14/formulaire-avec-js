dd03197729165bb12f6b490dd18e2bfd98063e85
iMac G1 <imacg1@iMac-de-Energy-23.local>
2022-05-06 13:45:27 +0000

    ajout du formulaire avecjs

---
+++ script.js
@@ -0,0 +1,22 @@
+function myfunction() {
+    let Name = document.querySelector("#Name");
+    let name = document.querySelector("#name");
+    Nom.innerHTML = Name.value;
+
+    let Email = document.querySelector("#Email");
+    let email = document.querySelector("#email");
+    email.innerHTML = Email.value;
+
+    let Satis = document.querySelector("#satis");
+    let satis = document.querySelector("#satisfaction");
+    satis.innerHTML = Satis.value;
+
+
+    let Coach = document.querySelector("#coachs");
+    let coach = document.querySelector("#coach");
+    coach.innerHTML = Coach.value;
+
+    let Experience = document.querySelector("#exp");
+    let exps= document.querySelector("#exps");
+    exps.innerHTML = Experience.value;
+}
